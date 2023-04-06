c5beabca6a1026d7d429de8bd69f9fa01c740f4b
Alal11 <104975763+Alal11@users.noreply.github.com>
2023-04-06 20:35:06 +0900

    아래 연산자와 수식 출력 결과 확인하기

---
+++ [2-1] 자바프로그래밍1 (김용강)/No2_4.java
@@ -0,0 +1,12 @@
+// 연산자와 수식
+// 아래 수식에 대한 결과 확인
+
+public class No2_4 {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println((true) && (3 > 4));
+        System.out.println(!(x > 0));
+        System.out.println((x != 0) || (x > 0));
+        System.out.println((x >= 0) || !(x == 1));
+    }
+}
