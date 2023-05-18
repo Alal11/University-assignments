4eab6234938c3e3b647510c75585ed578b131ccc
Alal11 <104975763+Alal11@users.noreply.github.com>
2023-05-18 13:21:49 +0900

    문자열 배열을 생성하고 배열 요소 출력하기

---
+++ [2-1] 자바프로그래밍1 (김용강)/No5_2.java
@@ -0,0 +1,12 @@
+// "Hello","Java", "World"를 가지고 있는 문자열의 배열 생성
+// 배열 요소 출력
+
+public class No5_2 {
+    public static void main(String[] args) {
+        String[] arr = {"Hello", "Java", "World"};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }
+}
